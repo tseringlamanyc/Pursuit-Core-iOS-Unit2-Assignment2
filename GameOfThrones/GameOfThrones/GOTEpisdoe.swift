06d664c3754bca5c8cf72cf9d5b1e12662e642f6
Tsering Lama <tseringlama@pursuit.org>
2019-11-18 22:36:24 -0500

    sorted the episodes by seasons

--- GameOfThrones/GameOfThrones/GOTEpisdoe.swift
+++ GameOfThrones/GameOfThrones/GOTEpisdoe.swift
@@ -98,4 +98,26 @@ class GOTEpisode {
         GOTEpisode(airdate: "2017-08-20", id: 1221414, name: "Beyond the Wall", number: 6, season: 7, runtime: 60, summary: "Jon's mission continues north of the wall, but the odds against his ragged band of misfits may be greater than he imagined.", mediumImageID: "312651", originalImageID: "312651"),
         GOTEpisode(airdate: "2017-08-27", id: 1221415, name: "The Dragon and the Wolf", number: 7, season: 7, runtime: 60, summary: "Cersei sits on the Iron Throne; Daenerys sails across the Narrow Sea; Jon Snow is King in the North, and winter is finally here.", mediumImageID: "314502", originalImageID: "314502")
     ]
+    
+   static func getSeasons() -> [[GOTEpisode]] {
+           let sortedSeasons = GOTEpisode.allEpisodes.sorted { $0.season < $1.season}
+           let seasonCount = Set<Int>(sortedSeasons.map {$0.season})
+           var arrArr = Array(repeating: [GOTEpisode](), count: seasonCount.count)
+           
+           var currentIndex = 0
+           var currentSection = sortedSeasons[currentIndex].season
+           
+           for season in sortedSeasons {
+               if season.season == currentSection {
+                   arrArr[currentIndex].append(season)
+               } else {
+                   currentIndex += 1
+                   currentSection = season.season
+                   arrArr[currentIndex].append(season)
+               }
+           }
+           
+           return arrArr
+       }
+    
 }
