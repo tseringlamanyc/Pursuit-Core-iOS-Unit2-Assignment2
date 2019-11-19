90e0555df669b6399884d293556a30b08d917d2a
Tsering Lama <tseringlama@pursuit.org>
2019-11-18 23:07:45 -0500

    added constraints on prototype cells

---
+++ GameOfThrones/GameOfThrones/DeatilsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DeatilsViewController.swift
+//  GameOfThrones
+//
+//  Created by Tsering Lama on 11/18/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class DeatilsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
